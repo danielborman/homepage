2ff32f28e8eeacc0fae39a78c06d564fecc8d827
Daniel Borman <daniel.borman@yahoo.com>
2019-11-06 19:55:54 -0800

    new bash

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
-top = open('template/top.html').read()
-index = open('content/index.html').read()
-projects = open('content/projects.html').read()
-blog = open('content/blog.html').read()
-bottom = open('template/bottom.html').read()
 
-index = top + index + bottom
-blog = top + blog + bottom
-projects = top + projects + bottom
-open('index.html', 'w+').write(art)
-open('projects.html', 'w+').write(blog)
-open('blog.html', 'w+').write(index)
 
+# BASH Script for Webpages
+
+cat top.html index.html bottom.html > docs/index.html
+cat top.html projects.html bottom.html > docs/projects.html
+cat top.html blog.html bottom.html > docs/blog.html
