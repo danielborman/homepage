022ff6538a8e1b63c9ae6d274ef89c8c8768dd59
Daniel Borman <daniel.borman@yahoo.com>
2019-11-07 17:48:27 -0800

    new py

---
+++ build.sh
@@ -0,0 +1,7 @@
+
+
+# BASH Script for Webpages
+
+cat top.html index.html bottom.html > docs/index.html
+cat top.html projects.html bottom.html > docs/projects.html
+cat top.html blog.html bottom.html > docs/blog.html
