cd5560656d061d637e6839f9f2cac22f54d5e3b8
Daniel Borman <daniel.borman@yahoo.com>
2019-11-11 12:35:21 -0800

    cleanup

--- build.sh
+++
@@ -1,7 +0,0 @@
-
-
-# BASH Script for Webpages
-
-cat top.html index.html bottom.html > docs/index.html
-cat top.html projects.html bottom.html > docs/projects.html
-cat top.html blog.html bottom.html > docs/blog.html
