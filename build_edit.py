ef1ebfec2dc8b2361eb4d09c3f28310384788f17
Daniel Borman <daniel.borman@yahoo.com>
2019-11-12 18:42:08 -0800

    delete

---
+++ build_edit.py
@@ -0,0 +1,17 @@
+def main():
+    top = open('templates/top.html').read()
+    index = open('content/index.html').read()
+    projects = open('content/projects.html').read()
+    blog = open('content/blog.html').read()
+    bottom = open('templates/bottom.html').read()
+
+
+
+
+index = top + index + bottom
+blog = top + blog + bottom
+projects = top + projects + bottom
+open('index.html', 'w+').write(index)
+open('projects.html', 'w+').write(projects)
+open('blog.html', 'w+').write(blog)
+
